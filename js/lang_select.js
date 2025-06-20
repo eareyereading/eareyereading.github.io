1d419fbdcabbb7ad194d69fc57240c92a132182f
胡永伟 <eareyereading@gmail.com>
2025-06-20 17:36:10 +0800

    Site updated: 2025-06-20 17:36:09

---
+++ js/lang_select.js
@@ -0,0 +1,1 @@
+(()=>{var a=window.Cookies.noConflict();function e(){var e=this.value,n=this.dataset.canonical,t="/";"zh-cn"!==e&&(t+=e+"/"),a.set("nf_lang",e,{expires:365}),location.href=t+n}document.getElementById("lang-select").addEventListener("change",e),document.getElementById("mobile-lang-select").addEventListener("change",e)})();
